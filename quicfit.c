abdae8d3fd34d9f9a62a9cba7547e25384fd595e
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-11-17 00:15:16 -0500

    Completed file input, calculates regression on input

--- quicfit.c
+++ quicfit.c
@@ -2,6 +2,7 @@
  * Copyright 2016 Ahnaf Siddiqui
  */
 
+#include <ctype.h>
 #include <float.h>
 #include <math.h>
 #include <stdio.h>
@@ -50,22 +51,26 @@ void fit2exp(const double x[], const double y[], int nx,
 
 
 void printDesc() {
-    printf("quicfit quickly fits a given set of points to a curve.\n");
-    printf("Currently, it can only fit to the two-parameter function (1-a)*exp(-x/b) + a\n\n");
+    fprintf(stderr, "quicfit quickly fits a given set of points to a curve.\n");
+    fprintf(stderr, "Currently, it can only fit to the two-parameter function (1-a)*exp(-x/b) + a\n\n");
 
-    printf("Usage: quicfit [OPTIONS] < FILE\n\n");
-    printf("The input file should have two columns of values (x y).\n");
-    printf("Lines starting with a non-numerical value are automatically ignored.\n\n");
+    fprintf(stderr, "Usage: quicfit [OPTIONS] < FILE\n\n");
+    fprintf(stderr, "The input file should have two columns of values (x y).\n");
+    fprintf(stderr, "Lines starting with a non-numerical value (besides whitespace and decimal point) "\
+                    "are automatically ignored.\n\n");
 
-    printf("Options:\n\n");
+    fprintf(stderr, "Options:\n\n");
 
-    printf("-a1\t(double) The first value for parameter a that should be tested during fitting.\n");
-    printf("-da\t(double) The interval between tested a values during fitting.\n");
-    printf("-na\t(int) The number of a values to test during fitting.\n\n");
+    fprintf(stderr, "-a1\t(double) The first value for parameter a that should be tested during fitting.\n");
+    fprintf(stderr, "-da\t(double) The interval between tested a values during fitting.\n");
+    fprintf(stderr, "-na\t(int) The number of a values to test during fitting.\n\n");
 
-    printf("-b1\t(double) The first value for parameter b that should be tested during fitting.\n");
-    printf("-db\t(double) The interval between tested b values during fitting.\n");
-    printf("-nb\t(int) The number of b values to test during fitting.\n\n");
+    fprintf(stderr, "-b1\t(double) The first value for parameter b that should be tested during fitting.\n");
+    fprintf(stderr, "-db\t(double) The interval between tested b values during fitting.\n");
+    fprintf(stderr, "-nb\t(int) The number of b values to test during fitting.\n\n");
+
+    fprintf(stderr, "-delim\t(string) The set of characters to use as delimiters between values "\
+                    "(x and y) in a row of the input file.\n");
 }
 
 
@@ -78,6 +83,9 @@ int main(int argc, char *argv[]) {
     double bbegin = 1.0;
     double db = 1.0;
     int nb = 1000;
+
+    // the delimiter between x and y in a row of the input file
+    const char *delims = " \t";
 
     // parse arguments
     for (int i = 1; i < argc; ++i) {
@@ -119,25 +127,82 @@ int main(int argc, char *argv[]) {
                 else if (strcmp(argv[i], "-nb") == 0) {
                     nb = atoi(arg);
                 }
+
+                // input delimiter
+                else if (strcmp(argv[i], "-delim") == 0) {
+                    delims = arg;
+                }
             }
         }
     }
 
 
     // DEBUG
-    // printf("%f\n", abegin);
-    // printf("%f\n", da);
-    // printf("%d\n", na);
-    // printf("%f\n", bbegin);
-    // printf("%f\n", db);
-    // printf("%d\n", nb);
+    // fprintf(stderr, "%f\n", abegin);
+    // fprintf(stderr, "%f\n", da);
+    // fprintf(stderr, "%d\n", na);
+    // fprintf(stderr, "%f\n", bbegin);
+    // fprintf(stderr, "%f\n", db);
+    // fprintf(stderr, "%d\n", nb);
 
-    // read stream input
     char buffer[BUFSIZ];
+    double *x = NULL;
+    double *y = NULL;
+    int nx = 0;
+    int alloc_size = 100;
 
-    while (fgets(buffer, sizeof buffer, stdin)) {
-        printf("%s", buffer);
+    // allocate memory for x and y values of input function
+    x = (double*)malloc(sizeof(double) * alloc_size);
+    y = (double*)malloc(sizeof(double) * alloc_size);
+
+    if (!x || !y) {
+        fputs("Memory error. Aborting.\n", stderr);
+        return 1;
     }
+
+    // read all the lines from the input stream
+    while (fgets(buffer, sizeof(buffer), stdin)) {
+        if (strlen(buffer) > 0) {
+            // Only process the current line if the first character
+            // is either a numerical digit, whitespace, or a decimal point.
+            // This is to allow for comments/other text in the input file.
+            if (isdigit(buffer[0]) || isspace(buffer[0]) || buffer[0] == '.') {
+                // get x and y from the current line of the input stream
+                const char *xstr = strtok(buffer, delims);
+                const char *ystr = NULL;
+                if (xstr) ystr = strtok(NULL, delims);
+
+                if (xstr && ystr) {
+                    if (nx >= alloc_size) {
+                        // reallocate memory if needed
+                        alloc_size *= 2;
+                        x = (double*)realloc(x, sizeof(double) * alloc_size);
+                        y = (double*)realloc(y, sizeof(double) * alloc_size);
+                    }
+
+                    x[nx] = atof(xstr);
+                    y[nx] = atof(ystr);
+
+                    ++nx;
+                }
+            }
+        }
+    }
+
+    // DEBUG
+    // for (int i = 0; i < nx; ++i) {
+    //     printf("%f\t%f\n", x[i], y[i]);
+    // }
+
+    // fit parameters and output results
+    double a, b;
+    fit2exp(x, y, nx, abegin, da, na, bbegin, db, nb, &a, &b);
+
+    printf("%f\n%f\n", a, b);
+
+
+    free(x);
+    free(y);
 
     return 0;
 }
